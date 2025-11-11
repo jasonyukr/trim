dfafee29b92684ac9f3114beaab67dbe7a8b0012
Jason Yu <jasonyu.kr@gmail.com>
2025-11-11 22:54:16 +0900

    Add --trail option

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,34 @@
 use std::io::{self, BufRead, BufWriter, Write};
 use std::process;
+use std::env;
 
 fn main() {
+    let trail = env::args().any(|arg| arg == "--trail");
+
     let stdout = io::stdout();
     let mut out = BufWriter::new(stdout);
     let stdin = io::stdin();
-    for line in stdin.lock().lines() {
-        if let Ok(ln) = line {
-            if let Err(_) = writeln!(out, "{}", ln.trim()) {
+    let mut stdin_lock = stdin.lock();
+
+    if trail {
+        let mut buf = String::new();
+        while let Ok(n) = stdin_lock.read_line(&mut buf) {
+            if n == 0 { break; }
+            if writeln!(out, "{}", buf.trim_end()).is_err() {
                 process::exit(1);
             }
+            buf.clear();
+        }
+    } else {
+        let mut buf = Vec::new();
+        while let Ok(n) = stdin_lock.read_until(b'\n', &mut buf) {
+            if n == 0 { break; }
+            if out.write_all(&buf).is_err() {
+                process::exit(1);
+            }
+            buf.clear();
         }
     }
+
     out.flush().unwrap();
 }
